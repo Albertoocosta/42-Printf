ea189f52c54b2da2ef8427ed655582b294de6c14
Alberto Costa <albertoocostapt@gmail.com>
2024-05-02 17:10:07 +0100

    FEAT: add some funtions to ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,53 +6,51 @@
 /*   By: cda-fons <cda-fons@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 15:40:54 by cda-fons          #+#    #+#             */
-/*   Updated: 2024/05/01 16:27:15 by cda-fons         ###   ########.fr       */
+/*   Updated: 2024/05/02 16:29:15 by cda-fons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf(const char *content, ...)
+int   checkArgs(const char *content, va_list args)
 {
-	va_list args;
    int   i;
 
    i = 0;
-   va_start(args, content);
    while (content[i])
    {
       if (content[i] == '%')
       {
          if (content[i + 1] == 's')
-         {
-            ft_putstr_fd(args, 1);
-         }
+            return (ft_putstr_fd(va_arg(args, char *), 1));
          if (content[i + 1] == 'c')
-         {
-            ft_putchar_fd(args, 1);
-         }
+            return (ft_putchar_fd(va_arg(args, char), 1));
          if (content[i + 1] == 'd')
-         {
-            ft_putnbr_fd(args, 1);
-         }
+            return (ft_putnbr_fd(va_arg(args, int), 1));
          if (content[i + 1] == 'i')
-         {
-            ft_putnbr_fd(args, 1);
-         }
+            return (ft_putnbr_fd(va_arg(args, int), 1));
          if (content[i + 1] == '%')
-         {
-            ft_putchar_fd("%", 1);
-         }
+            return (ft_putchar_fd(content[i + 1], 1));
          if (content[i + 1] == 'X')
-         {
-            ft_putnbr_fd(args, 1);
-         }
+            return (ft_putnbr_fd(va_arg(args, int), 1));
          if (content[i + 1] == 'x')
-         {
-            ft_putnbr_fd(args, 1);
-         }
+            return (ft_putnbr_fd(va_arg(args, int), 1));
       }
-      i++;
+      i++;  
    }
    return (0);
+}
+
+int	ft_printf(const char *content, ...)
+{
+	va_list args;
+   va_start(args, content);
+   checkArgs(content, args);
+   va_end(args);
+   return (0);
+}
+
+int main()
+{
+   ft_printf("Teste aqui %s\n");
 }
